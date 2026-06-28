12c21272003a5a814df8e17853a991327b556281
wenderWRS4 <wenderrodriguesdesouza675@gmail.com>
2026-06-28 13:05:18 -0300

    Primeiro codigo

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from './componentes/Header'
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
---
+++ src/componentes/Header.jsx
@@ -0,0 +1,21 @@
+  import './Header.css'
+  export default function Header(){
+  return (
+    <header id="header">
+
+       <img src="" alt="logo" id="logo" />
+
+       <ul id="menu">
+         <li><a href="#">preços</a></li>
+         <li><a href="#">categorias</a></li>
+       </ul>
+
+      <ul>
+        <li><a href="#">pesquisa</a></li>
+        <li><a href="#">carrinho</a></li>
+        <li><a href="#">login</a></li>
+      </ul>
+     
+</header>
+  )
+}
