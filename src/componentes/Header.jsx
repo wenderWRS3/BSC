78a0a2e271ccba203bd324900e184d3421484292
wenderWRS4 <wenderrodriguesdesouza675@gmail.com>
2026-06-28 22:11:03 -0300

    header do BSC

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,19 +1,25 @@
   import './Header.css'
+  import { FiSearch } from "react-icons/fi";
+  import { FiShoppingCart } from "react-icons/fi";
+  import { FiUser } from "react-icons/fi";
+  import { MdOutlineAttachMoney } from "react-icons/md";
+  import { AiFillAppstore } from "react-icons/ai";
   export default function Header(){
   return (
     <header id="header">
-
-       <img src="" alt="logo" id="logo" />
-
+       <div id="logoo">
+         <h1 id="logo">BSC</h1>
+         <p><strong>B</strong>om <strong>S</strong>imples é <strong>C</strong>onfortavel</p>
+       </div>
        <ul id="menu">
-         <li><a href="#">preços</a></li>
-         <li><a href="#">categorias</a></li>
+         <li><a href="#"><MdOutlineAttachMoney /></a></li>
+         <li><a href="#"><AiFillAppstore /></a></li>
        </ul>
 
       <ul>
-        <li><a href="#">pesquisa</a></li>
-        <li><a href="#">carrinho</a></li>
-        <li><a href="#">login</a></li>
+        <li><a href="#"><FiSearch /></a></li>
+        <li><a href="#"><FiShoppingCart /></a></li>
+        <li><a href="#"><FiUser /></a></li>
       </ul>
      
 </header>
