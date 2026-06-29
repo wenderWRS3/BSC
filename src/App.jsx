176cd989df98385170166c9517c812976912594e
wenderWRS4 <wenderrodriguesdesouza675@gmail.com>
2026-06-28 22:11:21 -0300

    FOI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './componentes/Header'
+import Main from './componentes/Main'
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Main />
     </div>
   )
 }
---
+++ src/componentes/Main.jsx
@@ -0,0 +1,8 @@
+import './Main.css'
+ export default function Main() {
+  return (
+    <div id="main">
+      <h1>Main Component</h1>
+    </div>
+  );
+}
