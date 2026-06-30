a2b3a6ca28ff4b4f314a3868b6002c3236cfd08d
wenderWRS4 <wenderrodriguesdesouza675@gmail.com>
2026-06-29 22:02:20 -0300

    nao deu certo hoje

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from './componentes/Header'
 import Main from './componentes/Main'
+import Exercicio from './componentes/Exercicio'
+import Log from './paginalog/Log.jsx'
 
 export default function App() {
   return (
     <div>
       <Header />
       <Main />
+      <Exercicio />
+      <Log />
     </div>
   )
 }
---
+++ src/paginalog/Log.jsx
@@ -0,0 +1,7 @@
+import './Log.css';
+
+export default function Log() {
+return (
+    <div><h2>aaaaaaaaaa</h2></div>
+  );
+}
