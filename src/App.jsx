e49833e36800303466e7db6dc8dd514fbce2e202
wenderWRS4 <wenderrodriguesdesouza675@gmail.com>
2026-07-10 22:11:08 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Header from './componentes/Header'
 import Main from './componentes/Main'
 import Log from './paginalog/Log.jsx'
-import Exercicio from './componentes/Exercicio'
 export default function App() {
   return (
       <div>
       <Header />
       <Main />
-      <Exercicio />
       <Log />
     </div>
   )
