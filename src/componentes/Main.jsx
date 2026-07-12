87c83b2996b6a4f7b5219021f399d8f750cef799
wenderWRS4 <wenderrodriguesdesouza675@gmail.com>
2026-07-12 10:50:42 -0300

    main001

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -1,8 +1,22 @@
 import './Main.css'
- export default function Main() {
+export default function Main() {
   return (
     <div id="main">
-      
+      <div id='bankai'>
+        <div id='apresentaçao'>
+          <p id='aan'>Bem vindo</p>
+          <p id='aja'><strong className='qqq'>B</strong>om <strong className='qqq'>S</strong>imples <br /> <strong className='qqq'>C</strong>onfortavel</p>
+          <p id='ooo'>Peças sem exagero, feitas para quem vive a vida real.
+            <br />
+            Do mercado ao almoço em família, da rua ao sofá
+           </p>
+        </div>
+      </div>
+
+      <div id='proMasCom'>
+        <h3>aaaaa</h3>
+      </div>
     </div>
+
   );
 }
