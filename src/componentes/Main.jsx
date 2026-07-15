93fd7703f25920551848817b51b7cb83a2a76d37
wenderWRS4 <wenderrodriguesdesouza675@gmail.com>
2026-07-14 22:34:49 -0300

    main003

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -1,29 +1,34 @@
 import './Main.css'
 import img from "../img/sem-imagem.png"
 import Card from "../prosp/card"
+import Catalogo from "./cardRapido/Catalogo"
 export default function Main() {
   const numero = 100;
   return (
-    <div id="main">
-      <div id='bankai'>
-        <div id='apresentaçao'>
-          <p id='aan'>Bem vindo</p>
-          <p id='aja'><strong className='qqq'>B</strong>om <strong className='qqq'>S</strong>imples <br /> <strong className='qqq'>C</strong>onfortavel</p>
-          <p id='ooo'>Peças sem exagero, feitas para quem vive a vida real.
-            <br />
-            Do mercado ao almoço em família, da rua ao sofá
-          </p>
+    <div>
+      <div id="main">
+        <div id='bankai'>
+          <div id='apresentaçao'>
+            <p id='aan'>Bem vindo</p>
+            <p id='aja'><strong className='qqq'>B</strong>om <strong className='qqq'>S</strong>imples <br /> <strong className='qqq'>C</strong>onfortavel</p>
+            <p id='ooo'>Peças sem exagero, feitas para quem vive a vida real.
+              <br />
+              Do mercado ao almoço em família, da rua ao sofá
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div id='proMasCom'>
-        <div id='aaall'>
-          <Card img={img} className="center"/>
-          <Card img={img} className="center"/>
-          <Card img={img} className="center"/>
+        <div id='proMasCom'>
+          <div id='aaall'>
+            <Card img={img} className="center" />
+            <Card img={img} className="center" />
+            <Card img={img} className="center" />
+          </div>
         </div>
       </div>
+      
+     <Catalogo/>
+
     </div>
-
   );
 }
---
+++ src/componentes/cardRapido/Catalogo.jsx
@@ -0,0 +1,19 @@
+import './Catalogo.css'
+
+
+export default function Cardvenda(){
+ return(
+    <div id='lça'>
+        <hr id='tfd'/>
+     <div id='carrr'>
+        <ul id='categoria'>
+            <li className='elo'><a href="#" className="cata">Moletao</a></li>
+            <li className='elo'><a href="#" className="cata">Jeans</a></li>
+            <li className='elo'><a href="#" className="cata">Acessorios</a></li>
+            <li className='elo'><a href="#" className="cata">Roupa intima</a></li>
+            <li className='elo'><a href="#" className="cata">Vertidos</a></li>
+        </ul>
+     </div>
+    </div>
+ )
+}
